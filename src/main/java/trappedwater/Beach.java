e2c408c5677a94ff1279ed4d0805d9c56538f59c
Dmitry Malkov <meteese@mail.ru>
2018-12-31 13:14:05 +1000

    indentation to spaces

--- src/main/java/trappedwater/Beach.java
+++ src/main/java/trappedwater/Beach.java
@@ -3,62 +3,56 @@ package trappedwater;
 public class Beach {
 
     private final LithosphereSection[] litSections;
-	private final int m; // alias to litSections.length
-	
-	
-	public Beach(int[] _groundScheme) {
-		this.m = _groundScheme.length;
-		this.litSections = new LithosphereSection[m];
-		for (int i = 0; i < m; i++) {
-			this.litSections[i] = new LithosphereSection(_groundScheme[i]);
-		}
-	}
-	
-	
-	public void takeWaveFromLeft(int _waveHeight) {
-		boolean waveStillMoves = true;
-		for (int i = 0; i < m; i++) {
-			if (waveStillMoves) {
-				if (_waveHeight > this.litSections[i].groundHeight) {
-					this.litSections[i].setWaterHeight(_waveHeight);
-				} else {
-					waveStillMoves = false;
-				}
-			}
-		}
-	}
-	
-	
-	public void extraWaterPourOff() {
-		// Pour water on boarders
-		this.litSections[0].extraWaterPourOff((LithosphereSection) null);
-		this.litSections[m-1].extraWaterPourOff((LithosphereSection) null);
-		
-		// Pour water to the left
-    	for (int i = 1; i < m - 1; i++) {
-    		this.litSections[i].extraWaterPourOff(litSections[i - 1]);
-    	}
-    	
-    	// Pour water to the right
-    	for (int i = m - 2; i > 0; i--) {
-    		litSections[i].extraWaterPourOff(litSections[i + 1]);
-    	}
-	}
-	
-	
-	public int getTrappedWater() {
-    	int sumWater = 0;
-    	for (LithosphereSection litSection : this.litSections) {
-    		sumWater += litSection.getTrappedWater();
-    	}
-    	System.out.println("sumWater = " + sumWater);
+    private final int m; // alias to litSections.length
+
+    public Beach(int[] _groundScheme) {
+        this.m = _groundScheme.length;
+        this.litSections = new LithosphereSection[m];
+        for (int i = 0; i < m; i++) {
+            this.litSections[i] = new LithosphereSection(_groundScheme[i]);
+        }
+    }
+
+    public void takeWaveFromLeft(int _waveHeight) {
+        boolean waveStillMoves = true;
+        for (int i = 0; i < m; i++) {
+            if (waveStillMoves) {
+                if (_waveHeight > this.litSections[i].groundHeight) {
+                    this.litSections[i].setWaterHeight(_waveHeight);
+                } else {
+                    waveStillMoves = false;
+                }
+            }
+        }
+    }
+
+    public void extraWaterPourOff() {
+        // Pour water on boarders
+        this.litSections[0].extraWaterPourOff((LithosphereSection) null);
+        this.litSections[m - 1].extraWaterPourOff((LithosphereSection) null);
+
+        // Pour water to the left
+        for (int i = 1; i < m - 1; i++) {
+            this.litSections[i].extraWaterPourOff(litSections[i - 1]);
+        }
+
+        // Pour water to the right
+        for (int i = m - 2; i > 0; i--) {
+            litSections[i].extraWaterPourOff(litSections[i + 1]);
+        }
+    }
+
+    public int getTrappedWater() {
+        int sumWater = 0;
+        for (LithosphereSection litSection : this.litSections) {
+            sumWater += litSection.getTrappedWater();
+        }
         return sumWater;
     }
-	
-	
-	public void dry() {
-		for (int i = 0; i < this.m; i++) {
-			this.litSections[i].setWaterHeight(0);
-		}
-	}
+
+    public void dry() {
+        for (int i = 0; i < this.m; i++) {
+            this.litSections[i].setWaterHeight(0);
+        }
+    }
 }
--- src/main/java/trappedwater/LithosphereSection.java
+++ src/main/java/trappedwater/LithosphereSection.java
@@ -2,36 +2,32 @@ package trappedwater;
 
 public class LithosphereSection {
 
-	/* Height is above mean sea level in each case  */
+    /* Height is above mean sea level in each case */
     public final int groundHeight;
     private int waterHeight;
-    
-    
+
     public LithosphereSection(int _groundHeight) {
-    	this.groundHeight = _groundHeight;
+        this.groundHeight = _groundHeight;
         this.waterHeight = 0;
     }
-    
-    
+
     public void setWaterHeight(int _waterHeight) {
-    	this.waterHeight = _waterHeight;
+        this.waterHeight = _waterHeight;
     }
-    
-    
-	public void extraWaterPourOff(LithosphereSection... neighborVerticals) {
-		for (LithosphereSection neighbor : neighborVerticals) {
-			boolean seaIsNear = (neighbor == null);
-			if (seaIsNear) {
-				this.waterHeight = 0;
-				return;
-			} else {
-				int support = Integer.max(neighbor.waterHeight, neighbor.groundHeight);
-				this.waterHeight = Integer.min(this.waterHeight, support);
-			}
-		}
+
+    public void extraWaterPourOff(LithosphereSection... neighborVerticals) {
+        for (LithosphereSection neighbor : neighborVerticals) {
+            boolean seaIsNear = (neighbor == null);
+            if (seaIsNear) {
+                this.waterHeight = 0;
+                return;
+            } else {
+                int support = Integer.max(neighbor.waterHeight, neighbor.groundHeight);
+                this.waterHeight = Integer.min(this.waterHeight, support);
+            }
+        }
     }
-	
-	
+
     public int getTrappedWater() {
         int trappedWater = this.waterHeight - this.groundHeight;
         return (trappedWater > 0) ? trappedWater : 0;
